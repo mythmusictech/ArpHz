a0d4bd7aacf15c50f3cb74f82337c2f444fec64b
AidenAC <aidenc@umich.edu>
2024-06-27 01:47:06 -0400

    started basic GUI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,45 @@ ArpHzAudioProcessorEditor::ArpHzAudioProcessorEditor (ArpHzAudioProcessor& p)
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    
+    //Set rate knob
+    rateKnob.setSliderStyle(juce::Slider::Rotary);
+    rateKnob.setTextBoxStyle(juce::Slider::TextBoxBelow, false, 50, 20);
+    
+    //Set mode buttons
+    /*          OH NOOO.... OUR TABLE.... ITS BROKEN.....
+    for (int i = 0; i < 4; i++)
+    {
+        modeButtons.add(new juce::TextButton); //Modes: Up, Down, Up/Down, Down/Up
+    }
+    
+    modeButtons[0]->setButtonText("Up");
+    modeButtons[1]->setButtonText("Down");
+    modeButtons[2]->setButtonText("Up/Down");
+    modeButtons[4]->setButtonText("Down/Up");
+    */
+    
+    //Set labels
+    rateLabel.setText("Rate", juce::dontSendNotification);
+    rateLabel.setEditable(false);
+    
+    //modeLabel.setText("Mode", juce::dontSendNotification);
+    //modeLabel.setEditable(false);
+    
+    //Add all GUI components
+    addAndMakeVisible(&rateKnob);
+    addAndMakeVisible(&rateLabel);
+    
+    /*
+    for (int modeID = 0; modeID < modeButtons.size(); modeID++)
+    {
+        addAndMakeVisible(modeButtons[modeID]);
+    }
+    addAndMakeVisible(&modeLabel);
+    */
+    
+    //Set plugin window size
+    setSize (450, 250);
 }
 
 ArpHzAudioProcessorEditor::~ArpHzAudioProcessorEditor()
@@ -27,14 +65,30 @@ void ArpHzAudioProcessorEditor::paint (juce::Graphics& g)
 {
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (juce::FontOptions (15.0f));
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void ArpHzAudioProcessorEditor::resized()
 {
     // This is generally where you'll want to lay out the positions of any
     // subcomponents in your editor..
+    
+    //Layout values
+    auto marginX = getWidth() * 0.02;
+    auto marginY = getHeight() * 0.02;
+    auto knobSize = getWidth() * 0.3;
+    auto buttonWidth = getWidth() * 0.1;
+    auto buttonHeight = getHeight() * 0.1;
+    auto textWidth = 100;
+    auto textHeight = 50;
+    
+    /*
+    for (int buttonID = 0; buttonID < modeButtons.size(); buttonID++)
+    {
+        modeButtons[buttonID]->setBounds(buttonID * (buttonWidth + marginX) + marginX, 2 * marginY + textHeight, buttonWidth, buttonHeight);
+    }
+    modeLabel.setBounds(marginX, marginY, textWidth, textHeight);
+    */
+    
+    rateKnob.setBounds(getWidth() - knobSize - marginX, getHeight() - knobSize - marginY, knobSize, knobSize);
+    rateLabel.setBounds(getWidth() - knobSize - (4 * marginX), getHeight() - (knobSize / 2) - marginY, textWidth, textHeight);
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -28,6 +28,21 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     ArpHzAudioProcessor& audioProcessor;
+    
+    //GUI Components
+    juce::Slider rateKnob;
+    /*
+    juce::TextButton buttonUp;
+    juce::TextButton buttonDown;
+    juce::TextButton buttonUpDown;
+    juce::TextButton buttonDownUp;
+    */
+    
+    //juce::OwnedArray<juce::TextButton> modeButtons;
+    
+    //Labels
+    juce::Label rateLabel;
+    //juce::Label modeLabel;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ArpHzAudioProcessorEditor)
 };
